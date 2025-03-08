ac85f6d0d322eab58df15d3a065d3f7533d3885b
DK <don@promptnow.com>
2025-03-08 20:18:05 +0700

    Add auto-correct feature

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
     let currentQuestionIndex = 0;
-    let score = 0;
+    let score = 0
+    let level = 0;
+    let levelsCount = 0;
+    let questionsCount = 0;
     const quizieSubHeader = document.getElementById("quizie-sub-header");
     const questionContainer = document.getElementById("question-container");
     const questionElement = document.getElementById("question");
     const optionsElement = document.getElementById("options");
-    const nextButton = document.getElementById("next-button");
+    const nextButton = document.getElementById("btn-next");
+    const autoCorrectButton = document.getElementById("btn-auto-correct");
+    const cardLevel = document.getElementById("card-level");
 
     fetch("./public/content/questions.json")
-        .then(response => response.json())
+        .then(res => res.json())
         .then(data => {
             const questions = data.flatMap(category => category.questions);
+            questionsCount = questions.length;
             showQuestion(questions[currentQuestionIndex]);
-
+            showCard();
+            autoCorrectButton.addEventListener("click", () => {
+                const autoSelect = document.querySelector(`input[value="${questions[currentQuestionIndex].answer}"]`);
+                autoSelect.checked = true;
+            });
             nextButton.addEventListener("click", () => {
                 const selectedOption = document.querySelector(`input[name="option"]:checked`);
                 if (selectedOption) {
@@ -22,6 +32,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     currentQuestionIndex++;
                     if (currentQuestionIndex < questions.length) {
                         showQuestion(questions[currentQuestionIndex]);
+                        showCard();
                     } else {
                         localStorage.setItem("quizScore", score);
                         localStorage.setItem("totalQuestions", questions.length);
@@ -29,7 +40,6 @@ document.addEventListener("DOMContentLoaded", () => {
                     }
                 }
             });
-
             function showQuestion(question) {
                 questionElement.textContent = question.question;
                 optionsElement.innerHTML = '';
@@ -46,4 +56,23 @@ document.addEventListener("DOMContentLoaded", () => {
                 });
             }
         });
+
+    function showCard() {
+        fetch("./public/content/card-levels.json")
+        .then(res => res.json())
+        .then(data => {
+            levelsCount = Math.floor(questionsCount / data.length);
+            level = Math.floor(score / levelsCount);
+            if (level >= 4) {
+                level = 3;
+            } else if (level >= 3) {
+                level = 2;
+            }
+            const cardUri = data[level].uri;
+            getCard(cardUri);
+            function getCard(uri) {
+                cardLevel.style.backgroundImage = `url(${uri})`;
+            };
+        });
+    }
 });
