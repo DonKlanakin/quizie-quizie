41044b599cdbec3132ca43dfbd4f3e2e666ea894
DK <don@promptnow.com>
2025-03-12 06:32:24 +0700

    Fix premature questions set loading, rewrite rewarded card criteria

--- app.js
+++ app.js
@@ -14,13 +14,11 @@ const cardCommonCaterPillar = new Image();
 const cardRareCocoon = new Image();
 const cardEliteMagicalButterfly = new Image();
 const cardLegendaryFairyGoddess = new Image();
-let questions = "./public/content/questions-intermediate.json";
+let questions = "";
 let currentQuestionIndex = 0;
 let score = 0;
 let level = 0;
 let cardsCollection = [];
-let playerLevelsCount = 0;
-let cardLevelsCount = 0;
 let questionsCount = 0;
 let questionsList = [];
 
@@ -28,7 +26,7 @@ let questionsList = [];
     const logPrefix = "main";
     try {
         await preload();
-        await loadQuestions();
+
     } catch (error) {
         console.error(`${logPrefix} :: ${error}`);
     }
@@ -38,6 +36,7 @@ async function preload() {
     const logPrefix = "preload";
     try {
         await preloadCards(cards);
+
     } catch (error) {
         console.error(`${logPrefix} :: ${error}`);
     }
@@ -50,7 +49,6 @@ async function preloadCards(uri) {
         let res = await fetch(uri);
         let data = await res.json();
 
-        cardLevelsCount = data.length;
         let cardImages = [
             { card: cardCommonCaterPillar, uri: data[0].uri },
             { card: cardRareCocoon, uri: data[1].uri },
@@ -59,30 +57,29 @@ async function preloadCards(uri) {
         ];
 
         await Promise.all(
-            cardImages.map(
-                ({ card, uri }) =>
-                    new Promise((resolve) => {
-                        card.src = uriPrefix + uri;
-                        card.onload = () => resolve();
-                        card.onerror = () => resolve();
-                    })
+            cardImages.map(({ card, uri }) =>
+                new Promise((resolve) => {
+                    card.src = uriPrefix + uri;
+                    card.onload = resolve;
+                    card.onerror = resolve;
+                })
             )
         );
 
         cardsCollection = cardImages;
 
         cardBeginer.addEventListener("click", () => {
-            questions = "./public/content/questions-beginner.json"
+            questions = "./public/content/questions-beginner.json";
             processTransition();
         });
 
         cardIntermediate.addEventListener("click", () => {
-            questions = "./public/content/questions-intermediate.json"
+            questions = "./public/content/questions-intermediate.json";
             processTransition();
         });
 
         cardExpert.addEventListener("click", () => {
-            questions = "./public/content/questions-Expert.json"
+            questions = "./public/content/questions-Expert.json";
             processTransition();
         });
 
@@ -92,6 +89,8 @@ async function preloadCards(uri) {
             setTimeout(() => {
                 mainScreen.style.opacity = "1";
             }, 200);
+
+            loadQuestions();
         }
 
     } catch (error) {
@@ -110,22 +109,18 @@ async function loadQuestions() {
         localStorage.setItem("questionsCount", questionsCount);
 
         showQuestion(questionsList[currentQuestionIndex]);
-        showCard(level);
+        showCard();
 
         nextButton.addEventListener("click", () => {
-            const selectedOption = document.querySelector(
-                `input[name="option"]:checked`
-            );
+            const selectedOption = document.querySelector(`input[name="option"]:checked`);
             if (selectedOption) {
-                if (
-                    selectedOption.value === questionsList[currentQuestionIndex].answer
-                ) {
+                if (selectedOption.value === questionsList[currentQuestionIndex].answer) {
                     score++;
                 }
                 currentQuestionIndex++;
                 if (currentQuestionIndex < questionsList.length) {
                     showQuestion(questionsList[currentQuestionIndex]);
-                    showCard(level);
+                    showCard();
                 } else {
                     localStorage.setItem("quizScore", score);
                     localStorage.setItem("totalQuestions", questionsList.length);
@@ -140,6 +135,7 @@ async function loadQuestions() {
             );
             autoSelect.checked = true;
         });
+
     } catch (error) {
         console.error(`${logPrefix} :: ${error}`);
     }
@@ -148,25 +144,25 @@ async function loadQuestions() {
 function showCard() {
     const logPrefix = "showCard";
     try {
-        playerLevelsCount = Math.floor(
-            questionsCount / (cardLevelsCount === 0 ? 1 : cardLevelsCount)
-        );
-        level = Math.floor(
-            score / (playerLevelsCount === 0 ? 1 : playerLevelsCount)
-        );
+        // Exclude legendary card from distribution
+        const numCardLevels = cardsCollection.length - 1;
+        const levelThreshold = Math.floor(questionsCount / numCardLevels);
+        level = Math.floor(score / levelThreshold);
 
-        if (level >= cardLevelsCount) {
-            level = cardLevelsCount - 1;
-        } else if (level >= cardLevelsCount - 1) {
-            level = cardLevelsCount - 2;
+        // Ensure the last card is "Legendary Fairy Goddess" and only given to a full-scored player
+        if (score === questionsCount) {
+            level = numCardLevels;
+        } else if (level >= numCardLevels) {
+            level = numCardLevels - 1;
         }
 
-        localStorage.setItem("cardLevelsCount", cardLevelsCount);
+        localStorage.setItem("cardLevelsCount", numCardLevels);
         setCard(level);
 
         function setCard(level) {
             cardLevel.style.backgroundImage = `url(${cardsCollection[level].card.src})`;
         }
+
     } catch (error) {
         console.error(`${logPrefix} :: ${error}`);
     }
@@ -188,8 +184,8 @@ function showQuestion(question) {
             optionsElement.appendChild(optionElement);
         });
 
-        quizieSubHeader.innerHTML = `คำถามที่ <b>${currentQuestionIndex + 1
-            }</b> จาก <b>${questionsList.length}</b> คำถาม`;
+        quizieSubHeader.innerHTML = `คำถามที่ <b>${currentQuestionIndex + 1}</b> จาก <b>${questionsList.length}</b> คำถาม`;
+        
     } catch (error) {
         console.error(`${logPrefix} :: ${error}`);
     }
